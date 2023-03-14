26fb76b908eee81d13fd49260d20a918c96f60b3
7i7o <mparij@gmail.com>
2023-03-14 18:56:46 -0300

    fix button style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main className="w-screen h-screen flex justify-center items-center">
-      <button className="text-5xl bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+      <button className="text-5xl bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-3 px-8 border border-blue-500 hover:border-transparent rounded-full">
         Hello SVGies!
       </button>
     </main>
