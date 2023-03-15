3a8d9710f600a1c21b4eb2b66f9edda3b143aa29
7i7o <mparij@gmail.com>
2023-03-15 11:35:25 -0300

    add useIsMounted hook

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,42 @@
-import "./globals.css";
+"use client";
 
-export const metadata = {
-  title: "SVGies",
-  description: "Unique Visual Wallet Identifier",
-};
+import "./globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+
+// export const metadata = {
+//   title: "SVGies",
+//   description: "Unique Visual Wallet Identifier",
+// };
+
+// Rainbowkit config
+import {
+  darkTheme,
+  getDefaultWallets,
+  RainbowKitProvider,
+} from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { mainnet } from "wagmi/chains";
+// import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider } = configureChains(
+  [mainnet],
+  [
+    // jsonRpcProvider({ rpc: () => { return { http: process.env.NEXT_PUBLIC_RPC_KEY || "" }; }, }),
+    publicProvider(),
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "SVGies App",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 export default function RootLayout({
   children,
@@ -12,7 +45,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <title>SVGies</title>
+        <meta name="description" content="Unique Visual Wallet Identifier" />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains} theme={darkTheme()}>
+          <body>{children}</body>
+        </RainbowKitProvider>
+      </WagmiConfig>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
+"use client";
+
+import { useIsMounted } from "./hooks/useIsMounted";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const mounted = useIsMounted();
   return (
-    <main className="w-screen h-screen flex justify-center items-center">
-      <button className="text-5xl bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-3 px-8 border border-blue-500 hover:border-transparent rounded-full">
-        Hello SVGies!
-      </button>
+    <main className="w-screen h-screen flex flex-col justify-center">
+      <div className="w-full flex justify-end">
+        {mounted ? (
+          <ConnectButton showBalance={false} accountStatus="avatar" />
+        ) : null}
+      </div>
+      <div className="w-full h-full flex justify-center items-center">
+        <a className="text-5xl bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-3 px-8 border border-blue-500 hover:border-transparent rounded-full">
+          Hello SVGies!
+        </a>
+      </div>
     </main>
   );
 }
